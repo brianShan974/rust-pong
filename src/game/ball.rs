468000dd7bf1d4c97b9dd4a3531350e7b20efc9c
brianShan974 <2366910832@qq.com>
2024-01-22 09:39:06 +0800

    Now all integer constants are u32.

--- src/game/ball.rs
+++ src/game/ball.rs
@@ -9,17 +9,17 @@ use super::scene::PADDLE_MARGIN;
 use super::scene::SCREEN_HEIGHT;
 use super::scene::SCREEN_WIDTH;
 
-pub const DEFAULT_RADIUS: u16 = 3;
+pub const DEFAULT_RADIUS: u32 = 3;
 pub const DEFAULT_BALL_SPEED: f32 = (SCREEN_WIDTH / 200) as f32;
 
 pub struct Ball {
     pos: Vec2,
     vel: Vec2,
-    radius: u16,
+    radius: u32,
 }
 
 impl Ball {
-    pub fn new(pos: Vec2, vel: Vec2, radius: u16) -> Self {
+    pub fn new(pos: Vec2, vel: Vec2, radius: u32) -> Self {
         Self { pos, vel, radius }
     }
 
@@ -47,7 +47,7 @@ impl Ball {
         &self.vel
     }
 
-    pub fn get_radius(&self) -> u16 {
+    pub fn get_radius(&self) -> u32 {
         self.radius
     }
 
@@ -70,7 +70,7 @@ impl Ball {
                     self.pos.x = (PADDLE_MARGIN + DEFAULT_PADDLE_WIDTH + self.radius) as f32;
                 } else {
                     self.pos.x =
-                        (SCREEN_WIDTH - (PADDLE_MARGIN + DEFAULT_PADDLE_WIDTH + self.radius)) as f32
+                        (SCREEN_WIDTH - PADDLE_MARGIN - DEFAULT_PADDLE_WIDTH - self.radius) as f32
                 }
             }
             _ => {
@@ -85,6 +85,38 @@ impl Ball {
 
     pub fn update_pos(&mut self) {
         self.pos += &self.vel;
+    }
+
+    pub fn collides_with<'a>(&self, paddle: &'a Paddle) -> Option<Collision<'a>> {
+        use Collision::WithPaddle;
+
+        let &Vec2 {
+            x: paddle_pos_x,
+            y: paddle_pos_y,
+        } = paddle.get_pos();
+        let half_height: f32 = (paddle.get_height() / 2) as f32;
+        let half_width: f32 = (paddle.get_width() / 2) as f32;
+        let radius_as_f32 = self.radius as f32;
+
+        // here, bounds mean the bounds of the y-values, not what we see on the screen
+        // upper bound means that y-value shouldn't be greater than this value
+        // lower bound means that y-value shouldn't be less than this value
+        // on screen, upper bound is actually beneath lower bound
+        let y_lower_bound: f32 = paddle_pos_y - half_height - radius_as_f32;
+        let y_upper_bound: f32 = paddle_pos_y + half_height + radius_as_f32;
+
+        let x_lower_bound: f32 = paddle_pos_x - half_width;
+        let x_upper_bound: f32 = paddle_pos_x + half_width;
+
+        if self.pos.y > y_lower_bound
+            && self.pos.y < y_upper_bound
+            && self.pos.x > x_lower_bound
+            && self.pos.x < x_upper_bound
+        {
+            Some(WithPaddle(paddle))
+        } else {
+            None
+        }
     }
 }
 
